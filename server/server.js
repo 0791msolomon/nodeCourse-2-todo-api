8b4c6b9af69fe2927337028d641287723a8d0c88
Matt <0791msolomon@gmail.com>
2018-10-04 16:09:00 -0700

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.post("/todos", (req, res) => {
   todo
     .save()
     .then(response => {
-      res.send(response);
+      res.status(200).send(response);
     })
     .catch(err => {
       console.log(err);
@@ -26,3 +26,5 @@ app.post("/todos", (req, res) => {
 app.listen(3000, () => {
   console.log("listening on port 3000");
 });
+
+module.exports = { app };
